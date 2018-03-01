0b2c40bfe4e2aaea4cb5058c5807ec5f5e904682
vanpana <van.panaite@gmail.com>
2018-03-01 13:15:01 +0200

    read from file and created missing numbers

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
+import os
+
 from Controller.Controller import Controller
+from Domain.Problem import Problem
 from View.Console import Console
 
 if __name__ == '__main__':
-    controller = Controller("")
+    problem = Problem("Data/4x4.matrix")
+    controller = Controller(problem)
     view = Console(controller)
 
     view.run()
